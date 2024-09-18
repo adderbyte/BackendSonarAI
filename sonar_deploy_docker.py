6f403f709e8c5aeb77ae47ee1752e2854e2681dc
adderbyte <lukman.olagoke@epfl.ch>
2024-09-18 09:27:17 +0200

    updated logic

--- sonar_deploy_docker.py
+++ sonar_deploy_docker.py
@@ -1,6 +1,7 @@
 import os
 
 import openai
+from typing import Annotated
 
 from yival.logger.token_logger import TokenLogger
 from yival.schemas.experiment_config import MultimodalOutput
@@ -9,7 +10,7 @@ from yival.wrappers.string_wrapper import StringWrapper
 
 import ray
 from datasets import Audio, Dataset
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from ray import serve
 
@@ -23,14 +24,6 @@ from langchain_unstructured import UnstructuredLoader
 # 1: Define a FastAPI app and wrap it in a deployment with a route handler.
 app = FastAPI()
 
-
-
- """
- (1) Handle form data based on the web page
- (2) Use state monitoring to  summary is not called intil get data is called 
-
-
- """
 ## application root
 @app.get("/")
 def root():
@@ -49,15 +42,20 @@ def root():
 
 @serve.ingress(app)
 class Summarizer:
-    def __init__(self, podcast_length: int, expertise_level: str):
+    def __init__(self,msg:str, count:int):
+        '''
+
+        no need to initialize instance variales
+        '''
         # self._processor = AutoFeatureExtractor.from_pretrained(model)
         # self._model = Wav2Vec2ForSequenceClassification.from_pretrained(model)
-        self._podcast_length = None
-        self._expertise_level =  None
-        self._UploadDile =  None
+        # self._podcast_length = None
+        # self._expertise_level =  None
+        self._msg =  msg
+        self._count =  count
 
 
-     def is_pdf(file_path): 
+    def is_pdf(file_path): 
         """
         check if file path ends in .pdf
         """
@@ -81,33 +79,12 @@ class Summarizer:
     
         return article_dataset
 
-
-    ## this handles parameters from the form
-    @app.post("/submit/")
-    async def login(UploadFile: Annotated[File(...), Form()], podcast_length: Annotated[intr, Form()],
-     expertise_level: Annotated[str, Form()]):
-         self._podcast_length = podcast_length
-         self._expertise_level =  expertise_level
-         self._UploadDile =  UploadFile
-         
-          # return {"username": username, expertise_level,podcast_length }
-    
-    ## this handles parameters from the form
-    @app.post("/submit-json/")
-    async def login_json(UploadFile: file = Body(...)  ,podcast_length: str = Body(...), expertise_level: str = Body(...)):
-       print("password", password)
-       self._podcast_length = podcast_length
-       self._expertise_level =  expertise_level
-       self._UploadDile =  UploadFile
-       #return return {"username": username, expertise_level,podcast_length }
-
-    @app.get("/get_summary")
-    def summarize(self,article: str, state: ExperimentState) -> MultimodalOutput:
+    def articleSummarizer(self,article: str, state: ExperimentState, podcast_length:int, expertise_level:str) -> MultimodalOutput:
 
         ### temp_file loator
         # Save the audio file to a temporary location
 
-        temp_file_path = f"temp/{datafile.filename}"
+        # temp_file_path = f"temp/{datafile.filename}"
         ## logging
         logger = TokenLogger()
         logger.reset()
@@ -129,8 +106,8 @@ class Summarizer:
              (2) Instructions: Begin with a powerful hook or intriguing question to immediately \
                        capture the listener's attention. Avoid too many podcast norms and go right into telling the engaging story.  \
                        Make sure that you acknowledge the source materials and authors by name.   \
-                       Weave the document content into a cohesive narrative appropriate for the  podcast length of {self._podcast_length} minutes.   \
-                       Use language and concepts suitable for an audience with {self._expertise_level} expertise \
+                       Weave the document content into a cohesive narrative appropriate for the  podcast length of {podcast_length} minutes.   \
+                       Use language and concepts suitable for an audience with {expertise_level} expertise \
                        Incorporate storytelling elements such as anecdotes, vivid descriptions, and dynamic pacing to maintain engagement. \
                        Conclude with a memorable ending that reinforces the key insights or prompts the audience to reflect further.\n  \
              (3) Guidelines: Engagement: Keep the tone lively and captivating to sustain interest throughout.   \
@@ -144,7 +121,7 @@ class Summarizer:
             )
         }, {
             "role": "user",
-            "content": self.get_response(temp_file_path) #article
+            "content": article
         }]
         # Use the chat-based completion
         response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
@@ -158,6 +135,28 @@ class Summarizer:
         return answer
 
 
+    @app.post("/submitform/")
+    async def login(datafile: Annotated[UploadFile, File(...)], podcast_length: Annotated[int, Form()],
+     expertise_level: Annotated[str, Form()], link_name: Annotated[str, Form()]):
+    
+         join_str = self._msg + str(self._count+1)
+         print("troubleshot message: ..." + join_str)
+
+         if not link_name.strip() : ### check f string nma eis empty
+            temp_file_path =  f"temp/{datafile.filename}"
+         else:
+            temp_file_path =  link_name
+         
+         ### get the file and load as pdf etc
+         article = self.get_response(temp_file_path)
+
+         answer = articleSummarizer(article=article, podcast_length=podcast_length,expertise_level=expertise_level )
+
+         return answer
+
+
+
+
 
 
 ray.init(_node_ip_address="0.0.0.0", ignore_reinit_error=True)
@@ -167,7 +166,7 @@ serve.start(
     http_options={"host": "0.0.0.0", "port": int(os.environ.get("PORT", "8000"))},
 )
 
-lid_inference = Summarizer()
+lid_inference = Summarizer("running .. .", 0)
 
 serve.run(lid_inference)
 
